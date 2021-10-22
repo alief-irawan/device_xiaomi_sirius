fa71c3ef99a2fcb32fb253ab63c2426e3579121f
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-10-22 19:00:00 +0000

    sirius: Build libjson from source Track https://github.com/LineageOS/android_external_json-c

--- sirius.mk
+++ sirius.mk
@@ -294,6 +294,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml
 
 # Radio
+$(call inherit-product, external/json-c/Android.configure.mk)
 PRODUCT_PACKAGES += \
     libjson \
     librmnetctl \
