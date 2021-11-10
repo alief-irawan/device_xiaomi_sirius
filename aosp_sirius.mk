563da1851ed2513d15a0f5794f0e596b579b33cc
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-11-10 04:55:11 +0000

    sirius: lineage -> aosp

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sirius
+PRODUCT_NAME := aosp_sirius
 PRODUCT_DEVICE := sirius
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
