2d73c3af5f498c055d51332f2ac55b29c84353b3
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2021-11-13 09:29:12 +0000

    sirius: Move to aosp build and add firmware to device mk

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_sirius
